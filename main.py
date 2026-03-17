a73d25721fa71c2ab92a663f8b42f40e3ed9ee80
KaRletka <muradnazarov2005@gmail.com>
2026-03-17 11:36:32 +0300

    Plug text

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 db_provider = DBProvider()
 app = FastAPI()
 
-PATH_TO_LOGFILE = "/var/lib/server_fastapi/dictionary.sqlite"
+PATH_TO_LOGFILE = "/var/lib/server_fastapi/api.log"
 # PATH_TO_LOGFILE = "api.log"
 
 logger = logging.getLogger("api")
